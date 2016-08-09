1a72b1db5624a08521c2a36861b0f3f18c1f6887
Ali Aminfar <ali.aminfar@gmail.com>
2016-08-09 23:43:15 +1000

    Removing GUI bars
    one line scrolling

---
+++ init.el
@@ -0,0 +1,13 @@
+;; Remove unncessary GUI bars
+(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+
+;; No splash screen
+(setq inhibit-startup-message t)
+
+;; scroll one line at a time (less "jumpy" than defaults)
+(setq mouse-wheel-scroll-amount '(1 ((shift) . 1))) ;; one line at a time
+(setq mouse-wheel-progressive-speed nil) ;; don't accelerate scrolling
+(setq mouse-wheel-follow-mouse 't) ;; scroll window under mouse
+(setq scroll-step 1) ;; keyboard scroll one line at a time
